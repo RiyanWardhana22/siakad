84cdf3a75e995510962e0174ca7d460029b739b7
riyanwardhana <riyanwardhana2@gmail.com>
2024-11-15 23:52:36 +0700

    install bootstrap v5.3

--- layout/_sidenav.php
+++ layout/_sidenav.php
@@ -11,23 +11,23 @@
     <ul class="sidebar-menu">
       <li class="menu-header">Dashboard</li>
       <li><a class="nav-link" href="../"><i class="fa fa-home" aria-hidden="true"></i><span>Home</span></a></li>
-      <li class="menu-header">Main Feature</li>
+      <li class="menu-header">Fitur Utama</li>
       <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fa fa-user" aria-hidden="true"></i> <span>Dosen</span></a>
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fa fa-pencil" aria-hidden="true"></i><span>Registrasi</span></a>
         <ul class="dropdown-menu">
           <li><a class="nav-link" href="../dosen/index.php">List</a></li>
           <li><a class="nav-link" href="../dosen/create.php">Tambah Data</a></li>
         </ul>
       </li>
       <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fa fa-address-card-o" aria-hidden="true"></i><span>Mahasiswa</span></a>
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fa fa-id-card" aria-hidden="true"></i><span>Perkuliahan</span></a>
         <ul class="dropdown-menu">
           <li><a class="nav-link" href="../mahasiswa/index.php">List</a></li>
           <li><a class="nav-link" href="../mahasiswa/create.php">Tambah Data</a></li>
         </ul>
       </li>
       <li class="dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Mata Kuliah</span></a>
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Ujian dan Nilai</span></a>
         <ul class="dropdown-menu">
           <li><a class="nav-link" href="../matakuliah/index.php">List</a></li>
           <li><a class="nav-link" href="../matakuliah/create.php">Tambah Data</a></li>
