29eb26077b11a6b98585c473d144716bc63889d5
riyanwardhana <riyanwardhana2@gmail.com>
2024-11-20 01:08:32 +0700

    fix: UI dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -2,13 +2,9 @@
 require_once '../layout/_top.php';
 require_once '../helper/connection.php';
 
-$mahasiswa = mysqli_query($connection, "SELECT COUNT(*) FROM mahasiswa");
-$dosen = mysqli_query($connection, "SELECT COUNT(*) FROM dosen");
 $matakuliah = mysqli_query($connection, "SELECT COUNT(*) FROM matakuliah");
-$nilai = mysqli_query($connection, "SELECT COUNT(*) FROM nilai");
+$nilai = mysqli_query($connection, "SELECT COUNT(*) FROM khs");
 
-$total_mahasiswa = mysqli_fetch_array($mahasiswa)[0];
-$total_dosen = mysqli_fetch_array($dosen)[0];
 $total_matakuliah = mysqli_fetch_array($matakuliah)[0];
 $total_nilai = mysqli_fetch_array($nilai)[0];
 ?>
@@ -36,61 +32,24 @@ $total_nilai = mysqli_fetch_array($nilai)[0];
         </div>
       </a>
       </div>
-      <a href="../mahasiswa/">
-      <div class="col-lg-5 col-md-6 col-sm-6 col-12">
-        <div class="card card-statistic-1">
-          <div class="card-icon bg-danger">
-            <i class="far fa-user"></i>
-          </div>
-          <div class="card-wrap">
-            <div class="card-header">
-              <h4>Total Mahasiswa</h4>
-            </div>
-            <div class="card-body">
-              <?= $total_mahasiswa ?>
-            </div>
-          </div>
-        </div>
-      </a>
-      </div>
-    </div>
-    <div class="row">
-      <a href="../matakuliah/">
+      <a href="../nilai/">
         <div class="col-lg-5 col-md-6 col-sm-6 col-12">
           <div class="card card-statistic-1">
-            <div class="card-icon bg-warning">
-              <i class="far fa-file"></i>
+            <div class="card-icon bg-success">
+              <i class="far fa-newspaper"></i>
             </div>
             <div class="card-wrap">
               <div class="card-header">
-                <h4>Total Mata Kuliah</h4>
+                <h4>Kartu Hasil Studi</h4>
               </div>
-              <div class="card-body">
-                <?= $total_matakuliah ?>
+            <div class="card-body">
+                <?= $total_nilai ?>
               </div>
             </div>
           </div>
         </a>
-        </div>
-        <a href="../nilai/">
-          <div class="col-lg-5 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-              <div class="card-icon bg-success">
-                <i class="far fa-newspaper"></i>
-              </div>
-              <div class="card-wrap">
-                <div class="card-header">
-                  <h4>Kartu Hasil Studi</h4>
-                </div>
-              <div class="card-body">
-                  <?= $total_nilai ?>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
+      </div>
     </div>
-  </div>
 </section>
 
 <?php
