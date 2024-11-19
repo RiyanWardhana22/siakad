8131e4dc77d841b1158fb53401e117198cbe9b61
riyanwardhana <riyanwardhana2@gmail.com>
2024-11-20 01:38:45 +0700

    cleancode in sidenav

--- layout/_sidenav.php
+++ layout/_sidenav.php
@@ -1,16 +1,16 @@
 <div class="main-sidebar sidebar-style-2">
   <aside id="sidebar-wrapper">
     <div class="sidebar-brand">
-      <a href="../">
+      <a href="../dashboard/">
         <img src="../assets/img/logo.png" alt="logo" width="200">
       </a>
     </div>
     <div class="sidebar-brand sidebar-brand-sm">
-      <a href="index.php">EF</a>
+      <a href="index.php">RW</a>
     </div>
     <ul class="sidebar-menu">
       <li class="menu-header">Dashboard</li>
-      <li><a class="nav-link" href="../"><i class="fa fa-home"></i><span>Home</span></a></li>
+      <li><a class="nav-link" href="../dashboard/"><i class="fa fa-home"></i><span>Home</span></a></li>
       <li class="menu-header">Fitur Utama</li>
       <li class="dropdown">
         <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-pencil-alt"></i> <span>Registrasi</span></a>
@@ -22,7 +22,7 @@
       <li class="dropdown">
         <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fa fa-id-card"></i><span>Perkuliahan</span></a>
         <ul class="dropdown-menu">
-          <li><a class="nav-link" href="../mahasiswa/index.php">List</a></li>
+          <li><a class="nav-link" href="../mahasiswa/">List</a></li>
           <!-- <li><a class="nav-link" href="../mahasiswa/create.php">Tambah Data</a></li> -->
         </ul>
       </li>
@@ -36,7 +36,7 @@
       <li class="dropdown">
         <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fa fa-file" aria-hidden="true"></i> <span>Ujian dan Nilai</span></a>
         <ul class="dropdown-menu">
-          <li><a class="nav-link" href="../nilai/index.php">◉ Kartu Hasil Studi(KHS)</a></li>
+          <li><a class="nav-link" href="../nilai/">◉ Kartu Hasil Studi(KHS)</a></li>
         </ul>
       </li>
     </ul>
