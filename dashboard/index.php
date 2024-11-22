30da268836bfc257ee63ffae9d105a35f1f972b0
riyanwardhana <riyanwardhana2@gmail.com>
2024-11-22 10:54:24 +0700

    add: card in dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,9 +4,11 @@ require_once '../helper/connection.php';
 
 $matakuliah = mysqli_query($connection, "SELECT COUNT(*) FROM matakuliah");
 $nilai = mysqli_query($connection, "SELECT COUNT(*) FROM khs");
+$krs = mysqli_query($connection, "SELECT COUNT(*) FROM krs");
 
 $total_matakuliah = mysqli_fetch_array($matakuliah)[0];
 $total_nilai = mysqli_fetch_array($nilai)[0];
+$total_krs = mysqli_fetch_array($krs)[0];
 ?>
 
 <section class="section ">
@@ -48,6 +50,23 @@ $total_nilai = mysqli_fetch_array($nilai)[0];
             </div>
           </div>
         </a>
+      </div>
+      <div class="col-lg-5 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <a href="../krs/">
+          <div class="card-icon bg-warning">
+          <i class="far fa-id-card"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Kartu Rencana Studi</h4>
+            </div>
+            <div class="card-body">
+              <?= $total_krs ?>
+            </div>
+          </div>
+        </div>
+      </a>
       </div>
     </div>
 </section>
